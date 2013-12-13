9c74169caceee0b52a1f1ebd30c70012150760dc
Robert <raedwards@wpi.edu>
2013-12-12 20:51:25 -0500

    Working group detection, updated functions

--- Final_Project/src/team2/scripts/PartyServer.py
+++ Final_Project/src/team2/scripts/PartyServer.py
@@ -26,10 +26,13 @@ from nav_msgs.msg import Odometry
 from std_msgs.msg import Empty
 from kobuki_msgs.msg import BumperEvent
 
+staticMapGrid = OccupancyGrid()
+localCostMapGrid = OccupancyGrid()
+globalCostMapGrid = OccupancyGrid()
+
+
 class Global:
     def __init__(self):
-        self.staticMapGrid = OccupancyGrid()
-        self.costMapGrid = OccupancyGrid()
         self.threshold = 75
         self.groupCount = 0
         self.allGroups = []
@@ -40,35 +43,48 @@ class Global:
     
     #evaluatePoints: can be used to find points containing obstacles in the global costmap
     def evaluatePoints(self):
-        mapWidth = self.staticMapGrid.info.width
-        mapHeight = self.staticMapGrid.info.height
-        mapOriginX = int(math.floor(self.costMapGrid.info.origin.position.x * 20))
-        mapOriginY = int(math.floor(self.costMapGrid.info.origin.position.y * 20))
+        mapWidth = staticMapGrid.info.width
+        mapHeight = staticMapGrid.info.height
+        mapOriginX = int(math.floor(globalCostMapGrid.info.origin.position.x * 20))
+        mapOriginY = int(math.floor(globalCostMapGrid.info.origin.position.y * 20))
+        
+        localMapWidth = localCostMapGrid.info.width
+        localMapHeight = localCostMapGrid.info.height
+        localMapOriginX = int(math.floor(globalCostMapGrid.info.origin.position.x * 20))
+        localMapOriginY = int(math.floor(globalCostMapGrid.info.origin.position.y * 20))
+        
         potentialPoints = []
-        for i in range(0, mapWidth):
-            for j in range(0, mapHeight):
-                if self.costMapGrid.data[(i * mapWidth) + j] > self.threshold:
+        
+        for i in range(0, localMapWidth):
+            for j in range(0, localMapHeight):
+                if localCostMapGrid.data[(i * localMapWidth) + j] > self.threshold:
                     staticValue = 0
                     for k in range(-7, 8):
                         for l in range(-7, 8):
-                            if self.staticMapGrid.data[((i + k) * mapWidth) + j + l] > 0:
+                            if staticMapGrid.data[((i - localMapOriginX + k) * mapWidth) + j - localMapOriginY + l] > 0:
                                 staticValue = 1
                                 break
                         else:
                             continue
                         break
                     if staticValue == 0:
-                        potentialPoints.append(Cell(j + mapOriginX, i + mapOriginY))
+                        potentialPoints.append(Cell(j + localMapOriginX, i + localMapOriginY))
         print len(potentialPoints)
         return potentialPoints
     
     #evaluateGroups:
     def evaluateGroups(self, potentialPoints):
         
-        mapWidth = self.costMapGrid.info.width
-        mapHeight = self.costMapGrid.info.height
-        mapOriginX = int(math.floor(self.costMapGrid.info.origin.position.x * 20))
-        mapOriginY = int(math.floor(self.costMapGrid.info.origin.position.y * 20))
+        mapWidth = globalCostMapGrid.info.width
+        mapHeight = globalCostMapGrid.info.height
+        mapOriginX = int(math.floor(globalCostMapGrid.info.origin.position.x * 20))
+        mapOriginY = int(math.floor(globalCostMapGrid.info.origin.position.y * 20))
+        
+        localMapWidth = localCostMapGrid.info.width
+        localMapHeight = localCostMapGrid.info.height
+        localMapOriginX = int(math.floor(globalCostMapGrid.info.origin.position.x * 20))
+        localMapOriginY = int(math.floor(globalCostMapGrid.info.origin.position.y * 20))
+        
         for i in range(0, len(potentialPoints)):
             #If there are no groups, create one
             if len(self.allGroups) == 0:
@@ -109,11 +125,13 @@ class Global:
         
         #calculate the center of each of the groups
         for n in range(0, len(self.allGroups)):
-            for o in range(0, len(self.allGroups[n].cells)):
-                if self.costMapGrid.data[((self.allGroups[n].cells[len(self.allGroups[n].cells) - 1 - o].y + mapOriginY) * mapWidth) + 
-                                    self.allGroups[n].cells[len(self.allGroups[n].cells) - 1 - o].x + mapOriginX] < self.threshold:
-                    del self.allGroups[n].cells[len(self.allGroups[n].cells) - 1 - o]
-            self.allGroups[n].computeCenterofGroup()
+            indices = []
+            for o in xrange(len(self.allGroups[n].cells) - 1, -1, -1):
+                if localCostMapGrid.data[((self.allGroups[n].cells[o].y - localMapOriginY) * localMapWidth) + 
+                                    self.allGroups[n].cells[o].x - localMapOriginX] < self.threshold:
+                    del self.allGroups[n].cells[o]
+        
+            #self.allGroups[n].computeCenterofGroup()
 
 g = Global()
 
@@ -137,57 +155,63 @@ class Group:
         self.centerX = sumXpos / len(self.cells)
         self.centerY = sumYpos / len(self.cells)
 
-class Subscriber:
+#class Subscriber:
     
-    def __init__(self):
-        self.getOdomData()
-        self.getStaticMapData()
-        self.getGlobalCostmapData()
+   # def __init__(self):
+   #     self.getOdomData()
+   #     self.getStaticMapData()
+   #     self.getGlobalCostmapData()
     #Subscriber Functions
-    def getOdomData(data):
-        sub = rospy.Subscriber("/odom", Odometry, odomCallback)
+def getOdomData():
+    sub = rospy.Subscriber("/odom", Odometry, odomCallback)
     
-    def getStaticMapData(data):
-        sub = rospy.Subscriber("/map", OccupancyGrid, staticMapCallBack)
+def getStaticMapData():
+    sub = rospy.Subscriber("/map", OccupancyGrid, staticMapCallBack)
         
-    def getGlobalCostmapData(data):
-        sub = rospy.Subscriber("/move_base/global_costmap/costmap", OccupancyGrid, globalCostmapCallBack)
-    
-    
+def getGlobalCostmapData():
+    sub = rospy.Subscriber("/move_base/global_costmap/costmap", OccupancyGrid, globalCostmapCallBack)
 
-class Publisher:
+def getLocalCostmapData():
+    sub = rospy.Subscriber("/move_base/local_costmap/costmap", OccupancyGrid, localCostmapCallBack)
+    
+#class Publisher:
     #Publisher Functions
     #publishGoal: publishes the x position, y position and angle for the robot to move to
-    def publishGoal(xPos, yPos, angle):
-        pub = rospy.Publisher('/move_base_simple/goal', PoseStamped)
-        goal = PoseStamped()
-        goal.pose.position.x = xPos
-        goal.pose.position.y = yPos
+def publishGoal(xPos, yPos, angle):
+    pub = rospy.Publisher('/move_base_simple/goal', PoseStamped)
+    goal = PoseStamped()
+    goal.pose.position.x = xPos
+    goal.pose.position.y = yPos
         #determine theta eventually
-        pub.publish(goal)
+    pub.publish(goal)
     
-    def publishCells(self, listOfCells):
-        pub = rospy.Publisher('/team2/cells_with_obstructions', GridCells)
-        cells = GridCells()
-        cells.header.frame_id = 'map'
-        cells.cell_width = 0.05
-        cells.cell_height = 0.05
-        for i in range(0, len(listOfCells)):
-            point = Point()
-            point.x = (listOfCells[i].x) * 0.05 + 0.025
-            point.y = (listOfCells[i].y) * 0.05 + 0.025
-            point.z = 0
-            cells.cells.append(point)
-        pub.publish(cells)
-        rospy.sleep(0.5)     
+def publishCells(listOfCells):
+    pub = rospy.Publisher('/team2/cells_with_obstructions', GridCells)
+    cells = GridCells()
+    cells.header.frame_id = 'map'
+    cells.cell_width = 0.05
+    cells.cell_height = 0.05
+    for i in range(0, len(listOfCells)):
+        point = Point()
+        point.x = (listOfCells[i].x) * 0.05 + 0.025
+        point.y = (listOfCells[i].y) * 0.05 + 0.025
+        point.z = 0
+        cells.cells.append(point)
+    pub.publish(cells)  
 
 #Subscriber Callback functions
 def staticMapCallBack(data):
-    global g
-    g.staticMapGrid = data
+    global staticMapGrid
+    staticMapGrid = data
+    
+def localCostmapCallBack(data):
+    global localCostMapGrid
+    localCostMapGrid = data
+    
 def globalCostmapCallBack(data):
-    global g
-    g.costMapGrid = data
+    global globalCostMapGrid
+    globalCostMapGrid = data
+    
 def odomCallback(data):
     global g
     px = data.pose.pose.position.x
@@ -212,23 +236,30 @@ class Cell:
             return False 
 
 if __name__ == '__main__':
-    rospy.init_node('lab5', anonymous=True)
+    rospy.init_node('PartyServer', anonymous=True)
     global g
-    g = Global()
-    s = Subscriber()
-    p = Publisher()
-    while(len(g.staticMapGrid.data) == 0 or
-          len(g.costMapGrid.data) == 0):
+    getOdomData()
+    getStaticMapData()
+    getLocalCostmapData()
+    getGlobalCostmapData()
+    #p = Publisher()
+    while(len(staticMapGrid.data) == 0 or
+          len(globalCostMapGrid.data) == 0):
         rand  = 0
-    print len(g.staticMapGrid.data)
-    print g.staticMapGrid.info.width
+    #print len(staticMapGrid.data)
+    #print staticMapGrid.info.width
     while(1):
         cells = g.evaluatePoints()
-        p.publishCells(cells)
-        for i in range(0, len(cells)):
-            print '[', cells[i].x, ' ', cells[i].y, ']'
+        publishCells(cells)
+        #for i in range(0, len(cells)):
+        #    print '[', cells[i].x, ' ', cells[i].y, ']'
         print len(cells)
-        oldData = g.costMapGrid.data
-        while oldData == g.costMapGrid.data:
+        oldData = localCostMapGrid.data
+        g.evaluateGroups(cells)
+        print len(g.allGroups)
+        while oldData == localCostMapGrid.data:
             rand = 1
+            
+            
+        print 'Change'
         
